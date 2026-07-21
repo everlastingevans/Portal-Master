e4d86185d791bc6791507d6afbcc532e414bf1bd
Evans Munatsa <evansmunatsa7@gmail.com>
2026-07-21 16:07:01 +0200

    temp price

--- app/api/jobs/checkout/route.ts
+++ app/api/jobs/checkout/route.ts
@@ -34,7 +34,7 @@ export async function GET(req: Request) {
 
     const payfastData = PayfastService.generatePaymentData(
       `JOB_${job.id}`,
-      1999,
+      19,
       `LaunchPath Job Posting: ${job.title}`,
       `${origin}/employer/payment/success?jobId=${job.id}`,
       `${origin}/employer/payment?jobId=${job.id}`,
